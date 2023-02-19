d945af403395aa470a753e3c31ecda4c5d7f7a1c
Alexander-Solonar <Alexander-Solonar_solo991@ukr.net>
2023-02-19 22:13:31 +0200

    finish task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const bodyEl = document.body;
+let intervalTime = null;
+btnStop.disabled = true;
 
+btnStart.addEventListener('click', onChangeColor);
+
+btnStop.addEventListener('click', onStopItervalTime);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onChangeColor() {
+  intervalTime = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+}
+
+function onStopItervalTime() {
+  clearInterval(intervalTime);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+}
